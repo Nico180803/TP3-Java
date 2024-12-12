d8e58b19539a35bee4c484b391d10e1b3d02dfa3
Nicolas CHARPENTIER <CHARPENTIER_Ni@lprs.local>
2024-12-12 15:28:32 +0100

    feat:morpion

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,151 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.xml.transform.Source;
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //VARIABLE
+        String Joueur1;
+        String Joueur2;
+        String [][] Plateau=new String[3][3];
+        int colonne;
+        int ligne;
+        boolean jouer;
+        String rejouer;
+        int tour;
+        int nombreVictoireJ1;
+        int nombreVictoireJ2;
+        int nombreEgalite;
+
+        //CODE
+
+        tour=0;
+        nombreVictoireJ1=0;
+        nombreVictoireJ2=0;
+        nombreEgalite=0;
+
+        do {
+
+
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    Plateau[i][j] = "-";
+                }
+            }
+
+
+            System.out.println("Bienvenu dans ce jeu du Morpion,");
+            System.out.println("Joueur 1, veuillez entrer votre Pseudo");
+            Joueur1 = sc.nextLine();
+            System.out.println("Joueur 2, veuillez entrer votre Pseudo");
+            Joueur2 = sc.nextLine();
+
+
+            System.out.println(Plateau[0][0] + " " + Plateau[0][1] + " " + Plateau[0][2]);
+            System.out.println(Plateau[1][0] + " " + Plateau[1][1] + " " + Plateau[1][2]);
+            System.out.println(Plateau[2][0] + " " + Plateau[2][1] + " " + Plateau[2][2]);
+
+            jouer = true;
+            while (jouer == true) {
+
+                System.out.println(Joueur1+" , parties gagné : "+nombreVictoireJ1);
+                System.out.println(Joueur2+" , parties gagné : "+nombreVictoireJ2);
+                System.out.println("Nombre d'égalité : "+nombreEgalite);
+
+                //JOUEUR 1
+                if (tour%2==0) {
+                    System.out.println("C'est à " + Joueur1);
+                }else {
+                    System.out.println("C'est à " + Joueur2);
+                }
+                System.out.println("Où voulez vous placer votre X ? Entrez le numéro de la ligne puis de la collone");
+                System.out.println("Position de la ligne : ");
+                ligne = sc.nextInt();
+                sc.nextLine();
+                System.out.println("Position de la colonne : ");
+                colonne = sc.nextInt();
+                sc.nextLine();
+                Plateau[ligne - 1][colonne - 1] = "X";
+
+                System.out.println(Plateau[0][0] + " " + Plateau[0][1] + " " + Plateau[0][2]);
+                System.out.println(Plateau[1][0] + " " + Plateau[1][1] + " " + Plateau[1][2]);
+                System.out.println(Plateau[2][0] + " " + Plateau[2][1] + " " + Plateau[2][2]);
+
+                if ((Plateau[0][0].equals("X") && Plateau[0][1].equals("X") && Plateau[0][2].equals("X")) ||
+                        (Plateau[1][0].equals("X") && Plateau[1][1].equals("X") && Plateau[1][2].equals("X")) ||
+                        (Plateau[2][0].equals("X") && Plateau[2][1].equals("X") && Plateau[2][2].equals("X")) ||
+                        (Plateau[0][0].equals("X") && Plateau[1][0].equals("X") && Plateau[2][0].equals("X")) ||
+                        (Plateau[0][1].equals("X") && Plateau[1][1].equals("X") && Plateau[2][1].equals("X")) ||
+                        (Plateau[0][2].equals("X") && Plateau[1][2].equals("X") && Plateau[2][2].equals("X")) ||
+                        (Plateau[2][0].equals("X") && Plateau[1][1].equals("X") && Plateau[0][2].equals("X")) ||
+                        (Plateau[0][0].equals("X") && Plateau[1][1].equals("X") && Plateau[2][2].equals("X"))) {
+                    if (tour%2==0) {
+                        System.out.println("Félicitation, " + Joueur1+" a gagné");
+                        nombreVictoireJ1+=1;
+                    }else {
+                        System.out.println("Félicitation, " + Joueur2+" a gagné");
+                        nombreVictoireJ2+=1;
+                    }
+                    jouer = false;
+                    break;
+                }
+
+                //JOUEUR2
+                if (tour%2==0) {
+                    System.out.println("C'est à " + Joueur2);
+                }else {
+                    System.out.println("C'est à " + Joueur1);
+                }
+                System.out.println("Où voulez vous placer votre O ? Entrez le numéro de la ligne puis de la collone");
+                System.out.println("Position de la ligne : ");
+                ligne = sc.nextInt();
+                sc.nextLine();
+                System.out.println("Position de la colonne : ");
+                colonne = sc.nextInt();
+                sc.nextLine();
+                Plateau[ligne - 1][colonne - 1] = "O";
+
+                System.out.println(Plateau[0][0] + " " + Plateau[0][1] + " " + Plateau[0][2]);
+                System.out.println(Plateau[1][0] + " " + Plateau[1][1] + " " + Plateau[1][2]);
+                System.out.println(Plateau[2][0] + " " + Plateau[2][1] + " " + Plateau[2][2]);
+
+                if ((Plateau[0][0].equals("O") && Plateau[0][1].equals("O") && Plateau[0][2].equals("O")) ||
+                        (Plateau[1][0].equals("O") && Plateau[1][1].equals("O") && Plateau[1][2].equals("O")) ||
+                        (Plateau[2][0].equals("O") && Plateau[2][1].equals("O") && Plateau[2][2].equals("O")) ||
+                        (Plateau[0][0].equals("O") && Plateau[1][0].equals("O") && Plateau[2][0].equals("O")) ||
+                        (Plateau[0][1].equals("O") && Plateau[1][1].equals("O") && Plateau[2][1].equals("O")) ||
+                        (Plateau[0][2].equals("O") && Plateau[1][2].equals("O") && Plateau[2][2].equals("O")) ||
+                        (Plateau[2][0].equals("O") && Plateau[1][1].equals("O") && Plateau[0][2].equals("O")) ||
+                        (Plateau[0][0].equals("O") && Plateau[1][1].equals("O") && Plateau[2][2].equals("O"))) {
+                    if (tour%2==0) {
+                        System.out.println("Félicitation, " + Joueur2+" a gagné");
+                        nombreVictoireJ2+=1;
+                    }else {
+                        System.out.println("Félicitation, " + Joueur1+" a gagné");
+                        nombreVictoireJ1+=1;
+                    }
+                    jouer = false;
+                    break;
+                }
+
+                if ((Plateau[0][0].equals("X")||Plateau[0][0].equals("O")) &&
+                        (Plateau[0][1].equals("X")||Plateau[0][1].equals("O")) &&
+                        (Plateau[0][2].equals("X")||Plateau[0][2].equals("O")) &&
+                        (Plateau[1][0].equals("X")||Plateau[1][0].equals("O")) &&
+                        (Plateau[1][1].equals("X")||Plateau[1][1].equals("O")) &&
+                        (Plateau[1][2].equals("X")||Plateau[1][2].equals("O")) &&
+                        (Plateau[2][0].equals("X")||Plateau[2][0].equals("O")) &&
+                        (Plateau[2][1].equals("X")||Plateau[2][1].equals("O")) &&
+                        (Plateau[2][2].equals("X")||Plateau[2][2].equals("O"))){
+                    System.out.println("Match Nul !");
+                    jouer =false;
+                }
+            }
+            System.out.println("Voulez-vous rejouer ? (O/N)");
+            rejouer = sc.nextLine().toUpperCase();
+            tour=tour+1;
+        }while(rejouer.equals("O"));
     }
 }
